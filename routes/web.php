ec888565fdc0f5f3824874aab4fdab83b542679c
Wanglei <flaboy.cn@gmail.com>
2017-09-04 21:02:38 +0800

    airload http header

--- routes/web.php
+++ routes/web.php
@@ -15,6 +15,15 @@ Route::get('/', function () {
     return view('welcome');
 });
 
+Auth::routes();
 Admin::routes();
+
+Route::get('/profile', function () {
+    return redirect(Luban::config()->get('sso_url').'profile');
+});
+
+Route::get('/admin-site-menus', function(){
+	return [];
+})->middleware('auth');
 
 Route::get('/home', 'HomeController@index')->name('home');
